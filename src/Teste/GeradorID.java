75bc94ac742e200753bdc075afb27841d28d2235
matheusfelipefroehlich <73142267+matheusfelipefroehlich@users.noreply.github.com>
2020-11-22 20:27:43 -0300

    Update GeradorID.java

--- src/Teste/GeradorID.java
+++ src/Teste/GeradorID.java
@@ -3,6 +3,8 @@ package Teste;
 public class GeradorID {
 
 	public static int ID = 0;
+	public static  int IDPedidos;
+	public static int IDItens = 0;
 	
 	public int getId() {
 		 return ID;
@@ -10,4 +12,21 @@ public class GeradorID {
 	public int setId() {
 		return ID++;
 	}
+
+	public static int getIDPedidos() {
+		return IDPedidos;
+	}
+	public int setIDPedidos() {
+		return IDPedidos++;
+	}
+	public static int getIDItens() {
+		return IDItens;
+	}
+	public int setIDItens() {
+		return IDItens++;
+	}
+	
+	
+	
+
 }
