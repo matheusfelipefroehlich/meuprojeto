b0c610ff3ff27230f563b27c4492b771c6742a78
matheusfelipefroehlich <73142267+matheusfelipefroehlich@users.noreply.github.com>
2020-11-22 20:29:35 -0300

    Update ItensPedido.java

--- src/Teste/ItensPedido.java
+++ src/Teste/ItensPedido.java
@@ -2,5 +2,29 @@ package Teste;
 
 public class ItensPedido {
 	private String id;
+	private Produto[] itens;
+	
+	public ItensPedido() {
+		
+	}
+	
+	public ItensPedido(String id, Produto[] itens) {
+		super();
+		this.id = id;
+		this.itens = itens;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Produto[] getItens() {
+		return itens;
+	}
+	public void setItens(Produto[] itens) {
+		this.itens = itens;
+	}
 
 }
+
